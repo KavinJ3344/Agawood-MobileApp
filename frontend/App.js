9c5156d126aef7faf9fd10d92499642bf310d7ea
KavinJ3344 <152595436+KavinJ3344@users.noreply.github.com>
2025-10-25 17:03:06 +0530

    first commit

---
+++ frontend/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Agawood Mobile App 🌿</Text>
+      <Text>This is working on web and mobile!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ed3838ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
